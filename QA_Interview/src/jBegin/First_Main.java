de0ab9a839ea0c837fc1f69156a86cf15ce85899
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-23 00:47:25 +0530

    Update First_Main.java

--- QA_Interview/src/jBegin/First_Main.java
+++ QA_Interview/src/jBegin/First_Main.java
@@ -9,9 +9,10 @@ public class First_Main {
 		
 		for(int i = len-1; i>=0; i--) {
 			rev = rev + s.charAt(i);
-			System.out.println(rev);
+			//System.out.println(rev);
 		}
-
+		
+		System.out.println(rev);
 	}
 
 }
