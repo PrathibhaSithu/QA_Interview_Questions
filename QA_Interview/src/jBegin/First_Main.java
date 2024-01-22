26487a21e8ea30e7f8555d6fec0ba79d5c07f0cb
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-23 00:38:58 +0530

    Update First_Main.java

--- QA_Interview/src/jBegin/First_Main.java
+++ QA_Interview/src/jBegin/First_Main.java
@@ -3,7 +3,14 @@ package jBegin;
 public class First_Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		String s = "Selenium";
+		int len = s.length();
+		String rev ="";
+		
+		for(int i = len-1; i>=0; i--) {
+			rev = rev + s.charAt(i);
+			System.out.println(rev);
+		}
 
 	}
 
