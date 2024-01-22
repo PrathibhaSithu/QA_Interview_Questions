46ca68e83dfc85ce81e30dcb2ef3bd3179eb4aa1
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-23 00:54:03 +0530

    Update First_Main.java

--- QA_Interview/src/jBegin/First_Main.java
+++ QA_Interview/src/jBegin/First_Main.java
@@ -7,12 +7,18 @@ public class First_Main {
 		int len = s.length();
 		String rev ="";
 		
+		String sb = "String Buffer";
+		
 		// using for loop
 		for(int i = len-1; i>=0; i--) {
 			rev = rev + s.charAt(i);
 			//System.out.println(rev);
 		}
 		System.out.println(rev);
+		
+		// using string buffer
+		StringBuffer sb2 = new StringBuffer(sb);
+		System.out.println(sb2.reverse());
 	}
 
 }
