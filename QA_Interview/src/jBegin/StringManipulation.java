bc1540009614b04270597266a5d10ec4101d7612
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-23 22:38:57 +0530

    Update StringManipulation.java

--- QA_Interview/src/jBegin/StringManipulation.java
+++ QA_Interview/src/jBegin/StringManipulation.java
@@ -3,7 +3,11 @@ package jBegin;
 public class StringManipulation {
 
 	public static void main(String[] args) {
+		String str = "The Quick Brown Fox Jump Over The Lazy Dog";
 		
+		System.out.println(str.length());
+		System.out.println(str.charAt(5));
+		System.out.println(str.indexOf("T"));
 
 	}
 
