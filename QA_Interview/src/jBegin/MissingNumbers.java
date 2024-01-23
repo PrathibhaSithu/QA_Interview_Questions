f1f1d0e1e83f10be473de93a2ebfe2a463c6fc82
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-23 13:22:48 +0530

    Update MissingNumbers.java

--- QA_Interview/src/jBegin/MissingNumbers.java
+++ QA_Interview/src/jBegin/MissingNumbers.java
@@ -3,8 +3,25 @@ package jBegin;
 public class MissingNumbers {
 
 	public static void main(String[] args) {
+		int a[] = {1,2,3,5,6};
+		int sum = 0;
 		
+		for(int i = 0; i<a.length; i++) {
+			sum = sum+a[i];
+			//System.out.println(sum);
+		}
+		System.out.println("Sum of the given ints in the array :: " + sum);
+		
+		int sum1 = 0;
+		for(int j=1; j<=6; j++) {
+			sum1 = sum1+j;
+			//System.out.println(sum1);
+		}
+		System.out.println("Sum with the missing Number :: " + sum1);
 
+		int miss = sum1-sum;
+		
+		System.out.println("Missing Value is :: " + miss);
 	}
 
 }
