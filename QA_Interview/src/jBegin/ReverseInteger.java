5e723477b4f14d91c95c4639fe9b1fdb3750b51f
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-23 12:29:01 +0530

    Update ReverseInteger.java

--- QA_Interview/src/jBegin/ReverseInteger.java
+++ QA_Interview/src/jBegin/ReverseInteger.java
@@ -6,13 +6,18 @@ public class ReverseInteger {
 		int num = 123456;
 		int rev = 0;
 		
+		// using proper algo
 		while(num !=0) {
 			rev = rev*10 + num%10;
-			System.out.println(rev);
+			//System.out.println(rev);
 			num = num/10;
-			System.out.println(num);
+			//System.out.println(num);
 		}
 		System.out.println("Reverse Numbers Are :: " + rev);
+		
+		// using string buffer reverse method
+		int strRev = 4789556;
+		System.out.println(new StringBuffer(String.valueOf(strRev)).reverse());
 
 	}
 
