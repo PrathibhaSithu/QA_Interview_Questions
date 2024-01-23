a3ff08298b71c51f95e2cec0867e6a9d1cc470e8
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-23 21:15:14 +0530

    Update FindSmallLarge.java

--- QA_Interview/src/jBegin/FindSmallLarge.java
+++ QA_Interview/src/jBegin/FindSmallLarge.java
@@ -1,9 +1,26 @@
 package jBegin;
 
+import java.util.Arrays;
+
 public class FindSmallLarge {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int num[] = {1,-1,5,54,-89,-999,54,4848,6569};
+		int largest = num[0];
+		int smallest = num[0];
+		
+		for(int i =1; i<num.length; i++) {
+			if(num[i]>largest) {
+				largest = num[i];
+			}
+			else if(num[i]<smallest) {
+				smallest = num[i];
+			}
+			System.out.println("\n Given Array is : " + Arrays.toString(num));
+			System.out.println("Largest Number in the Array is : " + largest);
+			System.out.println("Smallest Number in the Array is : " + smallest);
+		}
+		
 
 	}
 
