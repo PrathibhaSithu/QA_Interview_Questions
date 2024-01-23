d8d2c96a9f3a855e56339f830dd7222e29311d96
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-23 09:24:02 +0530

    Update Junk_Remove.java

--- QA_Interview/src/jBegin/Junk_Remove.java
+++ QA_Interview/src/jBegin/Junk_Remove.java
@@ -3,7 +3,12 @@ package jBegin;
 public class Junk_Remove {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String rmv = "देखने के लिए धन्यवाद 感谢您观看 1234567890 Hello World";
+		
+		// Regular Expression [a-z A-Z 0-9]
+		rmv = rmv.replaceAll("[^a-zA-Z0-9]","");
+		System.out.println(rmv);
 
 	}
 
