5ed131b9c3a4d02fe9b21b434b79532db984fe37
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-23 09:54:18 +0530

    Update ReverseInteger.java

--- QA_Interview/src/jBegin/ReverseInteger.java
+++ QA_Interview/src/jBegin/ReverseInteger.java
@@ -3,7 +3,14 @@ package jBegin;
 public class ReverseInteger {
 
 	public static void main(String[] args) {
+		int num = 123456;
+		int rev = 0;
 		
+		while(num !=0) {
+			rev = rev*10 + num%10;
+			num = num/10;
+		}
+		System.out.println("Reverse Numbers Are :: " + rev);
 
 	}
 
