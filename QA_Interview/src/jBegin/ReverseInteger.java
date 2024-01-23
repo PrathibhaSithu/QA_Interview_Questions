fb84b5ea2dbff955c000a0628afe6c6fb37f9deb
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-23 09:52:04 +0530

    Update ReverseInteger.java

--- QA_Interview/src/jBegin/ReverseInteger.java
+++ QA_Interview/src/jBegin/ReverseInteger.java
@@ -3,22 +3,7 @@ package jBegin;
 public class ReverseInteger {
 
 	public static void main(String[] args) {
-		int s = 123456;
-		int len = s.length();
-		String rev ="";
 		
-		String sb = "String Buffer";
-		
-		// using for loop
-		for(int i = len-1; i>=0; i--) {
-			rev = rev + s.charAt(i);
-			//System.out.println(rev);
-		}
-		System.out.println(rev);
-		
-		// using string buffer
-		StringBuffer sb2 = new StringBuffer(sb);
-		System.out.println(sb2.reverse());
 
 	}
 
