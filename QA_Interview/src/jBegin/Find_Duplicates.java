4d2fd7df1d73330c45cb622d7ea5a8ecb67516f9
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-23 19:37:30 +0530

    Update Find_Duplicates.java

--- QA_Interview/src/jBegin/Find_Duplicates.java
+++ QA_Interview/src/jBegin/Find_Duplicates.java
@@ -3,8 +3,15 @@ package jBegin;
 public class Find_Duplicates {
 
 	public static void main(String[] args) {
+		String ProNames[]= {"java","C","java","javaScript","python","C"};
 		
-
+		for(int i=0; i<ProNames.length; i++){
+			for(int j = i+1; j<ProNames.length; j++){
+				if(ProNames[i].equals(ProNames[j])) {
+					System.out.println(ProNames[i]);
+				}
+			}
+		}
 	}
 
 }
