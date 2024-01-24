235e72147c1ffad929a4cd17aba47a0ec5140462
PrathiSithu <sithuminiprathiba@gmail.com>
2024-01-24 11:22:38 +0530

    Update StringManipulation.java

--- QA_Interview/src/jBegin/StringManipulation.java
+++ QA_Interview/src/jBegin/StringManipulation.java
@@ -4,13 +4,18 @@ public class StringManipulation {
 
 	public static void main(String[] args) {
 		String str = "The Quick Brown Fox Jump Over The Lazy Dog";
+		String str1 = "The Quick Brown Fox Jump Over the Lazy Dog";
 		
 		System.out.println(str.length());
 		System.out.println(str.charAt(5));
 		System.out.println(str.indexOf("T"));
 		System.out.println(str.indexOf("T",5));
-		System.out.println(str.indexOf("j",5)+1);
+		System.out.println(str.indexOf("J")+1);
 		System.out.println(str.indexOf("Fox"));
+		
+		System.out.println(str.equals(str1));
+		System.out.println(str.equalsIgnoreCase(str1));
+		System.out.println(str.substring(0, 8));
 	}
 
 }
